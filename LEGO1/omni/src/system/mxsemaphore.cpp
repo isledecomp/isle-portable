1a90e696699db9a0c392cfa5aadf2fea2f0a8f50
Christian Semmler <mail@csemmler.com>
2024-05-30 14:10:47 -0400

    Add SDL3 and replace Windows semaphores

--- LEGO1/omni/src/system/mxsemaphore.cpp
+++ LEGO1/omni/src/system/mxsemaphore.cpp
@@ -8,27 +8,30 @@ DECOMP_SIZE_ASSERT(MxSemaphore, 0x08)
 // FUNCTION: LEGO1 0x100c87d0
 MxSemaphore::MxSemaphore()
 {
-	m_hSemaphore = NULL;
+	m_semaphore = NULL;
 }
 
 // FUNCTION: LEGO1 0x100c8800
 MxResult MxSemaphore::Init(MxU32 p_initialCount, MxU32 p_maxCount)
 {
+	// [library:synchronization] No support for max count, but shouldn't be necessary
 	MxResult result = FAILURE;
-	if ((m_hSemaphore = CreateSemaphoreA(NULL, p_initialCount, p_maxCount, NULL))) {
+	if ((m_semaphore = SDL_CreateSemaphore(p_initialCount))) {
 		result = SUCCESS;
 	}
 	return result;
 }
 
 // FUNCTION: LEGO1 0x100c8830
-void MxSemaphore::Wait(MxU32 p_timeoutMS)
+void MxSemaphore::Wait()
 {
-	WaitForSingleObject(m_hSemaphore, p_timeoutMS);
+	// [library:synchronization] Removed timeout since only INFINITE is ever requested
+	SDL_WaitSemaphore(m_semaphore);
 }
 
 // FUNCTION: LEGO1 0x100c8850
-void MxSemaphore::Release(MxU32 p_releaseCount)
+void MxSemaphore::Release()
 {
-	ReleaseSemaphore(m_hSemaphore, p_releaseCount, NULL);
+	// [library:synchronization] Removed release count since only 1 is ever requested
+	SDL_PostSemaphore(m_semaphore);
 }
