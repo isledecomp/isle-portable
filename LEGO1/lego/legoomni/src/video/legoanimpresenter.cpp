30ce90dfb66183c534562c1d714e07c304dfe838
Christian Semmler <mail@csemmler.com>
2024-12-25 08:52:52 -0700

    Merge from isledecomp/isle

--- LEGO1/lego/legoomni/src/video/legoanimpresenter.cpp
+++ LEGO1/lego/legoomni/src/video/legoanimpresenter.cpp
@@ -655,9 +655,9 @@ void LegoAnimPresenter::PutFrame()
 
 					up -= m_currentWorld->GetCamera()->GetWorldLocation();
 					dir /= dirsqr;
-					pos.EqualsCross(&dir, &up);
+					pos.EqualsCross(dir, up);
 					pos.Unitize();
-					up.EqualsCross(&pos, &dir);
+					up.EqualsCross(pos, dir);
 					pos *= possqr;
 					dir *= dirsqr;
 					up *= upsqr;
