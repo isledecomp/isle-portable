ca825e804794502eae337b0395ecf8faf8bfd71e
Anders Jenbo <anders@jenbo.dk>
2025-05-21 21:29:54 +0200

    miniwin: Implement IDirect3DRMMesh

--- miniwin/miniwin/src/include/miniwin_d3drmmesh_p.h
+++ miniwin/miniwin/src/include/miniwin_d3drmmesh_p.h
@@ -2,33 +2,93 @@
 
 #include "miniwin_d3drmobject_p.h"
 
+#include <algorithm>
+#include <vector>
+
+struct MeshGroup {
+	D3DCOLOR color = 0xFFFFFFFF;
+	IDirect3DRMTexture* texture = nullptr;
+	IDirect3DRMMaterial* material = nullptr;
+	D3DRMRENDERQUALITY quality = D3DRMRENDER_GOURAUD;
+	int vertexPerFace = 0;
+	std::vector<D3DRMVERTEX> vertices;
+	std::vector<unsigned int> faces;
+
+	MeshGroup() = default;
+
+	MeshGroup(const MeshGroup& other)
+		: color(other.color), texture(other.texture), material(other.material), quality(other.quality),
+		  vertexPerFace(other.vertexPerFace), vertices(other.vertices), faces(other.faces)
+	{
+		if (texture) {
+			texture->AddRef();
+		}
+		if (material) {
+			material->AddRef();
+		}
+	}
+
+	// Move constructor
+	MeshGroup(MeshGroup&& other) noexcept
+		: color(other.color), texture(other.texture), material(other.material), quality(other.quality),
+		  vertexPerFace(other.vertexPerFace), vertices(other.vertices), faces(other.faces)
+	{
+		other.texture = nullptr;
+		other.material = nullptr;
+	}
+
+	// Move assignment
+	MeshGroup& operator=(MeshGroup&& other) noexcept
+	{
+		color = other.color;
+		texture = other.texture;
+		material = other.material;
+		quality = other.quality;
+		vertexPerFace = other.vertexPerFace;
+		vertices = other.vertices;
+		faces = other.faces;
+		other.texture = nullptr;
+		other.material = nullptr;
+		return *this;
+	}
+
+	~MeshGroup()
+	{
+		if (texture) {
+			texture->Release();
+		}
+		if (material) {
+			material->Release();
+		}
+	}
+};
+
 struct Direct3DRMMeshImpl : public Direct3DRMObjectBase<IDirect3DRMMesh> {
 	HRESULT Clone(int flags, GUID iid, void** object) override;
-	HRESULT GetBox(D3DRMBOX* box) override;
-	HRESULT AddGroup(int vertexCount, int faceCount, int vertexPerFace, void* faceBuffer, D3DRMGROUPINDEX* groupIndex)
+	HRESULT AddGroup(int vertexCount, int faceCount, int vertexPerFace, DWORD* faceBuffer, D3DRMGROUPINDEX* groupIndex)
 		override;
 	HRESULT GetGroup(
-		int groupIndex,
+		DWORD groupIndex,
 		DWORD* vertexCount,
 		DWORD* faceCount,
 		DWORD* vertexPerFace,
 		DWORD* dataSize,
 		DWORD* data
 	) override;
-	HRESULT SetGroupColor(int groupIndex, D3DCOLOR color) override;
-	HRESULT SetGroupColorRGB(int groupIndex, float r, float g, float b) override;
-	HRESULT SetGroupMaterial(int groupIndex, IDirect3DRMMaterial* material) override;
+	HRESULT SetGroupColor(DWORD groupIndex, D3DCOLOR color) override;
+	HRESULT SetGroupColorRGB(DWORD groupIndex, float r, float g, float b) override;
+	D3DCOLOR GetGroupColor(D3DRMGROUPINDEX index) override;
+	HRESULT SetGroupMaterial(DWORD groupIndex, IDirect3DRMMaterial* material) override;
+	HRESULT SetGroupTexture(DWORD groupIndex, IDirect3DRMTexture* texture) override;
+	HRESULT GetGroupTexture(DWORD groupIndex, LPDIRECT3DRMTEXTURE* texture) override;
 	HRESULT SetGroupMapping(D3DRMGROUPINDEX groupIndex, D3DRMMAPPING mapping) override;
-	HRESULT SetGroupQuality(int groupIndex, D3DRMRENDERQUALITY quality) override;
-	HRESULT SetVertices(int groupIndex, int offset, int count, D3DRMVERTEX* vertices) override;
-	HRESULT SetGroupTexture(int groupIndex, IDirect3DRMTexture* texture) override;
-	;
-	HRESULT GetGroupTexture(int groupIndex, LPDIRECT3DRMTEXTURE* texture) override;
-	D3DRMMAPPING GetGroupMapping(int groupIndex) override;
-	D3DRMRENDERQUALITY GetGroupQuality(int groupIndex) override;
-	HRESULT GetGroupColor(D3DRMGROUPINDEX index) override;
-	HRESULT GetVertices(int groupIndex, int startIndex, int count, D3DRMVERTEX* vertices) override;
+	D3DRMMAPPING GetGroupMapping(DWORD groupIndex) override;
+	HRESULT SetGroupQuality(DWORD groupIndex, D3DRMRENDERQUALITY quality) override;
+	D3DRMRENDERQUALITY GetGroupQuality(DWORD groupIndex) override;
+	HRESULT SetVertices(DWORD groupIndex, int offset, int count, D3DRMVERTEX* vertices) override;
+	HRESULT GetVertices(DWORD groupIndex, int startIndex, int count, D3DRMVERTEX* vertices) override;
+	HRESULT GetBox(D3DRMBOX* box) override;
 
 private:
-	IDirect3DRMTexture* m_groupTexture = nullptr;
+	std::vector<MeshGroup> m_groups;
 };
