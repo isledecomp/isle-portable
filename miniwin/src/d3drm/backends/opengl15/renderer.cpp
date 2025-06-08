d860b05dee1952176c4b32c748447d9a9248f73c
Anders Jenbo <anders@jenbo.dk>
2025-06-08 23:26:45 +0200

    SDL3 GPU texture support
    Co-authored-by: Anonymous Maarten <anonymous.maarten@gmail.com>

--- miniwin/src/d3drm/backends/opengl15/renderer.cpp
+++ miniwin/src/d3drm/backends/opengl15/renderer.cpp
@@ -176,7 +176,7 @@ Uint32 OpenGL15Renderer::GetTextureId(IDirect3DRMTexture* iTexture)
 
 	for (Uint32 i = 0; i < m_textures.size(); ++i) {
 		auto& tex = m_textures[i];
-		if (tex.texture == nullptr) {
+		if (!tex.texture) {
 			tex.texture = texture;
 			tex.version = texture->m_version;
 			tex.glTextureId = texId;
@@ -335,7 +335,8 @@ void OpenGL15Renderer::SubmitDraw(
 
 	if (appearance.flat) {
 		glShadeModel(GL_FLAT);
-	} else {
+	}
+	else {
 		glShadeModel(GL_SMOOTH);
 	}
 
