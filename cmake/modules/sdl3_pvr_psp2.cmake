33e59f103a06dccaac56d16c3e0ff95b19964a06
olebeck <31539311+olebeck@users.noreply.github.com>
2025-06-22 13:52:53 +0200

    use proper cmake patch for sdl

--- cmake/modules/sdl3_pvr_psp2.cmake
+++ cmake/modules/sdl3_pvr_psp2.cmake
@@ -9,25 +9,13 @@ FetchContent_Declare(
     GIT_REPOSITORY "https://github.com/libsdl-org/SDL.git"
     GIT_TAG "main"
     EXCLUDE_FROM_ALL
+    PATCH_COMMAND git apply "${SDL3_PATCH_FILE}"
 )
-
-FetchContent_GetProperties(SDL3)
-
-if(NOT SDL3_POPULATED)
-  FetchContent_Populate(SDL3)
-  execute_process(
-    COMMAND git apply --verbose "${SDL3_PATCH_FILE}"
-    WORKING_DIRECTORY "${sdl3_SOURCE_DIR}"
-    RESULT_VARIABLE APPLY_RESULT
-  )
-  if(NOT APPLY_RESULT EQUAL 0)
-    message(FATAL_ERROR "Failed to apply patch to SDL3")
-  endif()
-endif()
 
 block()
   set(VIDEO_VITA_PVR ON)
   get_target_property(PVR_INCLUDES GLESv2 INTERFACE_INCLUDE_DIRECTORIES)
   list(APPEND CMAKE_REQUIRED_INCLUDES ${PVR_INCLUDES})
-  add_subdirectory(${sdl3_SOURCE_DIR} ${sdl3_BINARY_DIR})
+  FetchContent_MakeAvailable(SDL3)
+  target_include_directories(SDL3-static PRIVATE ${PVR_INCLUDES})
 endblock()
