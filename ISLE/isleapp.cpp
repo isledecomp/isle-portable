baba3c7682844ad9bc2f89b4e9e25fb7ee404cfc
Helloyunho <me@helloyunho.xyz>
2025-07-06 20:36:54 +0900

    🩹 fix: click trigger spam clicks

--- ISLE/isleapp.cpp
+++ ISLE/isleapp.cpp
@@ -554,7 +554,7 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 			g_lastJoystickMouseY = ((MxFloat) axisValue) / SDL_JOYSTICK_AXIS_MAX * g_isle->GetCursorSensitivity();
 		}
 		else if (event->gaxis.axis == SDL_GAMEPAD_AXIS_RIGHT_TRIGGER) {
-			if (axisValue != 0) {
+			if (axisValue != 0 && !g_mousedown) {
 				g_mousedown = TRUE;
 
 				if (InputManager()) {
@@ -567,7 +567,7 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 					);
 				}
 			}
-			else {
+			else if (axisValue == 0 && g_mousedown) {
 				g_mousedown = FALSE;
 
 				if (InputManager()) {
