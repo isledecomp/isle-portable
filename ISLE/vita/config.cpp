0efac83ac5c7a7d977359cd4009712733903eb87
olebeck <31539311+olebeck@users.noreply.github.com>
2025-07-07 15:02:57 +0200

    move CONFIG_vita

--- ISLE/vita/config.cpp
+++ ISLE/vita/config.cpp
@@ -7,11 +7,8 @@ void VITA_SetupDefaultConfigOverrides(dictionary* p_dictionary)
 {
 	SDL_Log("Overriding default config for VITA");
 
-	iniparser_set(p_dictionary, "isle:diskpath", "ux0:isledecompile/isle/disk");
-	iniparser_set(p_dictionary, "isle:cdpath", "ux0:isledecompile/isle/cd");
+	iniparser_set(p_dictionary, "isle:diskpath", "ux0:data/isledecompile/isle/disk");
+	iniparser_set(p_dictionary, "isle:cdpath", "ux0:data/isledecompile/isle/cd");
 	iniparser_set(p_dictionary, "isle:UseJoystick", "true");
 	iniparser_set(p_dictionary, "isle:Draw Cursor", "true");
-
-	// Use e_noAnimation/cut transition
-	// iniparser_set(p_dictionary, "isle:Transition Type", "1");
 }
