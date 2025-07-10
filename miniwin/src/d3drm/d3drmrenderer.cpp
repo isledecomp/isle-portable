6754f93c436554869e075173329cab19e2e54440
olebeck <31539311+olebeck@users.noreply.github.com>
2025-07-10 18:10:25 +0200

    move backend selection and enum to its own functions

---
+++ miniwin/src/d3drm/d3drmrenderer.cpp
@@ -0,0 +1,76 @@
+#include "d3drmrenderer.h"
+#ifdef USE_OPENGL1
+#include "d3drmrenderer_opengl1.h"
+#endif
+#ifdef USE_OPENGLES2
+#include "d3drmrenderer_opengles2.h"
+#endif
+#ifdef USE_CITRO3D
+#include "d3drmrenderer_citro3d.h"
+#endif
+#ifdef USE_DIRECTX9
+#include "d3drmrenderer_directx9.h"
+#endif
+#ifdef USE_SDL_GPU
+#include "d3drmrenderer_sdl3gpu.h"
+#endif
+#ifdef USE_SOFTWARE_RENDER
+#include "d3drmrenderer_software.h"
+#endif
+
+Direct3DRMRenderer* CreateDirect3DRMRenderer(const DDSURFACEDESC& DDSDesc, const GUID* guid)
+{
+#ifdef USE_SDL_GPU
+	if (SDL_memcmp(guid, &SDL3_GPU_GUID, sizeof(GUID)) == 0) {
+		return Direct3DRMSDL3GPURenderer::Create(DDSDesc.dwWidth, DDSDesc.dwHeight);
+	}
+#endif
+#ifdef USE_SOFTWARE_RENDER
+	if (SDL_memcmp(guid, &SOFTWARE_GUID, sizeof(GUID)) == 0) {
+		return new Direct3DRMSoftwareRenderer(DDSDesc.dwWidth, DDSDesc.dwHeight);
+	}
+#endif
+#ifdef USE_OPENGLES2
+	if (SDL_memcmp(guid, &OpenGLES2_GUID, sizeof(GUID)) == 0) {
+		return OpenGLES2Renderer::Create(DDSDesc.dwWidth, DDSDesc.dwHeight);
+	}
+#endif
+#ifdef USE_OPENGL1
+	if (SDL_memcmp(guid, &OpenGL1_GUID, sizeof(GUID)) == 0) {
+		return OpenGL1Renderer::Create(DDSDesc.dwWidth, DDSDesc.dwHeight);
+	}
+#endif
+#ifdef USE_CITRO3D
+	if (SDL_memcmp(guid, &Citro3D_GUID, sizeof(GUID)) == 0) {
+		return new Citro3DRenderer(DDSDesc.dwWidth, DDSDesc.dwHeight);
+	}
+#endif
+#ifdef USE_DIRECTX9
+	if (SDL_memcmp(guid, &DirectX9_GUID, sizeof(GUID)) == 0) {
+		return DirectX9Renderer::Create(DDSDesc.dwWidth, DDSDesc.dwHeight);
+	}
+#endif
+	return nullptr;
+}
+
+void Direct3DRMRenderer_EnumDevices(LPD3DENUMDEVICESCALLBACK cb, void* ctx)
+{
+#ifdef USE_SDL_GPU
+	Direct3DRMSDL3GPU_EnumDevice(cb, ctx);
+#endif
+#ifdef USE_OPENGLES2
+	OpenGLES2Renderer_EnumDevice(cb, ctx);
+#endif
+#ifdef USE_OPENGL1
+	OpenGL1Renderer_EnumDevice(cb, ctx);
+#endif
+#ifdef USE_CITRO3D
+	Citro3DRenderer_EnumDevice(cb, ctx);
+#endif
+#ifdef USE_DIRECTX9
+	DirectX9Renderer_EnumDevice(cb, ctx);
+#endif
+#ifdef USE_SOFTWARE_RENDER
+	Direct3DRMSoftware_EnumDevice(cb, ctx);
+#endif
+}
