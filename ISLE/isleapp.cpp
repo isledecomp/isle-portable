77758b44317c69c29c79da1908b353e47fc4783f
VoxelTek <53562267+VoxelTek@users.noreply.github.com>
2025-07-16 10:16:40 +1000

    Remove unnecesary break
    Co-authored-by: Christian Semmler <mail@csemmler.com>

--- ISLE/isleapp.cpp
+++ ISLE/isleapp.cpp
@@ -324,7 +324,6 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv)
 			window
 			);
 			return SDL_APP_FAILURE;
-			break;
 		case SDL_APP_SUCCESS:
 			return SDL_APP_SUCCESS;
 			break;
