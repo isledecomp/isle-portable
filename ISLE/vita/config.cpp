7cfded0ea677fe8ccf3981b2f16c327d1f920942
olebeck <31539311+olebeck@users.noreply.github.com>
2025-08-12 19:58:56 +0200

    adjust diskpath to be like other ports

--- ISLE/vita/config.cpp
+++ ISLE/vita/config.cpp
@@ -7,6 +7,6 @@ void VITA_SetupDefaultConfigOverrides(dictionary* p_dictionary)
 {
 	SDL_Log("Overriding default config for VITA");
 
-	iniparser_set(p_dictionary, "isle:diskpath", "ux0:data/isledecompile/isle/disk");
-	iniparser_set(p_dictionary, "isle:cdpath", "ux0:data/isledecompile/isle/cd");
+	iniparser_set(p_dictionary, "isle:diskpath", "ux0:data/isledecompile/DATA/disk");
+	iniparser_set(p_dictionary, "isle:cdpath", "ux0:data/isledecompile/");
 }
