6f3f8cbc590625d147230366d8d7cc7c94e1dd64
VoxelTek <53562267+VoxelTek@users.noreply.github.com>
2025-08-14 13:17:11 +1000

    Begin work on adjustable key mapping

--- LEGO1/lego/legoomni/include/legoinputmanager.h
+++ LEGO1/lego/legoomni/include/legoinputmanager.h
@@ -12,6 +12,7 @@
 #include <SDL3/SDL_joystick.h>
 #include <SDL3/SDL_keyboard.h>
 #include <SDL3/SDL_keycode.h>
+#include <SDL3/SDL_scancode.h>
 #include <SDL3/SDL_timer.h>
 #ifdef MINIWIN
 #include "miniwin/windows.h"
@@ -87,7 +88,7 @@ public:
 		c_right = 0x02,
 		c_up = 0x04,
 		c_down = 0x08,
-		c_ctrl = 0x10,
+		c_sprint = 0x10,
 
 		c_leftOrRight = c_left | c_right,
 		c_upOrDown = c_up | c_down
@@ -99,6 +100,14 @@ public:
 		e_arrowKeys,
 		e_gamepad,
 	};
+
+	struct {
+		SDL_Scancode k_forward[2] = {SDL_SCANCODE_UP, SDL_SCANCODE_KP_8};
+		SDL_Scancode k_back[2] = {SDL_SCANCODE_DOWN, SDL_SCANCODE_KP_2};
+		SDL_Scancode k_left[2] = {SDL_SCANCODE_LEFT, SDL_SCANCODE_KP_4};
+		SDL_Scancode k_right[2] = {SDL_SCANCODE_RIGHT, SDL_SCANCODE_KP_6};
+		SDL_Scancode k_sprint[2] = {SDL_SCANCODE_LCTRL, SDL_SCANCODE_RCTRL};
+	} g_keyMaps;
 
 	LegoInputManager();
 	~LegoInputManager() override;
