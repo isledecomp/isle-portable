e709400dc627924b14ad41692a346220e1523dec
Christian Semmler <mail@csemmler.com>
2025-08-20 10:21:28 -0700

    Add new directives to SiLoader, allow specifying directives in `.ini` (#688)
    * WIP Prepend directive
    
    * WIP
    
    * Add new directives

--- extensions/include/extensions/siloader.h
+++ extensions/include/extensions/siloader.h
@@ -2,6 +2,7 @@
 
 #include "extensions/extensions.h"
 #include "legoworld.h"
+#include "mxactionnotificationparam.h"
 #include "mxatom.h"
 
 #include <map>
@@ -24,18 +25,25 @@ public:
 	static std::optional<MxResult> HandleStart(MxDSAction& p_action);
 	static std::optional<MxBool> HandleRemove(StreamObject p_object, LegoWorld* world);
 	static std::optional<MxBool> HandleDelete(MxDSAction& p_action);
+	static MxBool HandleEndAction(MxEndActionNotificationParam& p_param);
 
 	static std::map<std::string, std::string> options;
 	static std::vector<std::string> files;
+	static std::vector<std::string> directives;
 	static bool enabled;
 
 private:
 	static std::vector<std::pair<StreamObject, StreamObject>> startWith;
 	static std::vector<std::pair<StreamObject, StreamObject>> removeWith;
 	static std::vector<std::pair<StreamObject, StreamObject>> replace;
+	static std::vector<std::pair<StreamObject, StreamObject>> prepend;
+	static std::vector<StreamObject> fullScreenMovie;
+	static std::vector<StreamObject> disable3d;
 
 	static bool LoadFile(const char* p_file);
-	static void ParseDirectives(const MxAtomId& p_atom, si::Core* p_core, MxAtomId p_parentReplacedAtom = MxAtomId());
+	static bool LoadDirective(const char* p_directive);
+	static MxStreamController* OpenStream(const char* p_file);
+	static void ParseExtra(const MxAtomId& p_atom, si::Core* p_core, MxAtomId p_parentReplacedAtom = MxAtomId());
 };
 
 #ifdef EXTENSIONS
@@ -44,11 +52,13 @@ constexpr auto HandleFind = &SiLoader::HandleFind;
 constexpr auto HandleStart = &SiLoader::HandleStart;
 constexpr auto HandleRemove = &SiLoader::HandleRemove;
 constexpr auto HandleDelete = &SiLoader::HandleDelete;
+constexpr auto HandleEndAction = &SiLoader::HandleEndAction;
 #else
 constexpr decltype(&SiLoader::Load) Load = nullptr;
 constexpr decltype(&SiLoader::HandleFind) HandleFind = nullptr;
 constexpr decltype(&SiLoader::HandleStart) HandleStart = nullptr;
 constexpr decltype(&SiLoader::HandleRemove) HandleRemove = nullptr;
 constexpr decltype(&SiLoader::HandleDelete) HandleDelete = nullptr;
+constexpr decltype(&SiLoader::HandleEndAction) HandleEndAction = nullptr;
 #endif
 }; // namespace Extensions
