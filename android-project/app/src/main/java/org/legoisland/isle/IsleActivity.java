b9b74aec8f877a77c1f12d7903a9a51a508ddbdb
FilAgain2763 <zippernotfound@outlook.com>
2025-09-19 20:16:26 +0100

    This is NOT an Android port.

--- android-project/app/src/main/java/org/legoisland/isle/IsleActivity.java
+++
@@ -1,9 +0,0 @@
-package org.legoisland.isle;
-
-import org.libsdl.app.SDLActivity;
-
-public class IsleActivity extends SDLActivity {
-    protected String[] getLibraries() {
-        return new String[] { "SDL3", "lego1", "isle" };
-    }
-}
