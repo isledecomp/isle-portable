4a000cc3221f5460a2533e287abda85a2008ea65
SnepOMatic (Rhew) <github@rhew.co.uk>
2025-11-12 20:18:50 +0000

    Fix: base GetPathInfo fixup in the SDL implementation, with check for null SDL_PathInfo. Fix: Disable imgui shell functions.
    Todo: Display imgui debug if enabled.

--- ISLE/switch/filesys.cpp
+++ ISLE/switch/filesys.cpp
@@ -1,30 +1,41 @@
 #include <cstdio>
 #include <sys/stat.h>
-
+#include <dirent.h>
+#include <cerrno>
+#include <SDL3/SDL.h>
 #include "filesys.h"
 
+
+// Missing from Switch SDL3 implementation
+#ifndef S_ISREG
+#define S_ISREG(m) (((m) & _S_IFMT) == _S_IFREG)
+#endif
+
+// Uses core of SDL's SDL_GetPathInfo but handles null 'info'.
 bool NX_GetPathInfo(const char *path, SDL_PathInfo *info)
 {
-    struct stat st_info;
-    if (stat(path, &st_info) != 0)
-    {
-        if (info) info->type = SDL_PATHTYPE_NONE;
-        return false;
+    SDL_PathInfo tmp_info;
+    struct stat statbuf;
+    const int rc = stat(path, &statbuf);
+
+    if (rc < 0) {
+        return SDL_SetError("Can't stat: %s", strerror(errno));
+    } else if (S_ISREG(statbuf.st_mode)) {
+        tmp_info.type = SDL_PATHTYPE_FILE;
+        tmp_info.size = (Uint64) statbuf.st_size;
+    } else if (S_ISDIR(statbuf.st_mode)) {
+        tmp_info.type = SDL_PATHTYPE_DIRECTORY;
+        tmp_info.size = 0;
+    } else {
+        tmp_info.type = SDL_PATHTYPE_OTHER;
+        tmp_info.size = (Uint64) statbuf.st_size;
     }
 
-    if (st_info.st_mode & S_IFDIR)
-    {
-        if (info)info->type = SDL_PATHTYPE_DIRECTORY;
-        return true;
-    }
+    tmp_info.create_time = (SDL_Time)SDL_SECONDS_TO_NS(statbuf.st_ctime);
+    tmp_info.modify_time = (SDL_Time)SDL_SECONDS_TO_NS(statbuf.st_mtime);
+    tmp_info.access_time = (SDL_Time)SDL_SECONDS_TO_NS(statbuf.st_atime);
 
-    if (info) info->type = SDL_PATHTYPE_FILE;
-    if (!info) return true;
-
-    auto *fp = fopen(path, "r");
-    fseek(fp, 0, SEEK_END);
-    if (info) info->size = ftell(fp);
-    fclose(fp);
+    if (info) *info = tmp_info;
 
     return true;
 }
