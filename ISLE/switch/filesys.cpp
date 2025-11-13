957d53dc17a84640eda90e1f09672b4130825884
SnepOMatic (Rhew) <github@rhew.co.uk>
2025-11-13 09:54:44 +0000

    clang-format fix

--- ISLE/switch/filesys.cpp
+++ ISLE/switch/filesys.cpp
@@ -1,10 +1,10 @@
-#include <cstdio>
-#include <sys/stat.h>
-#include <dirent.h>
-#include <cerrno>
-#include <SDL3/SDL.h>
 #include "filesys.h"
 
+#include <SDL3/SDL.h>
+#include <cerrno>
+#include <cstdio>
+#include <dirent.h>
+#include <sys/stat.h>
 
 // Missing from Switch SDL3 implementation
 #ifndef S_ISREG
@@ -12,30 +12,35 @@
 #endif
 
 // Uses core of SDL's SDL_GetPathInfo but handles null 'info'.
-bool NX_GetPathInfo(const char *path, SDL_PathInfo *info)
+bool NX_GetPathInfo(const char* path, SDL_PathInfo* info)
 {
-    SDL_PathInfo tmp_info;
-    struct stat statbuf;
-    const int rc = stat(path, &statbuf);
+	SDL_PathInfo tmp_info;
+	struct stat statbuf;
+	const int rc = stat(path, &statbuf);
 
-    if (rc < 0) {
-        return SDL_SetError("Can't stat: %s", strerror(errno));
-    } else if (S_ISREG(statbuf.st_mode)) {
-        tmp_info.type = SDL_PATHTYPE_FILE;
-        tmp_info.size = (Uint64) statbuf.st_size;
-    } else if (S_ISDIR(statbuf.st_mode)) {
-        tmp_info.type = SDL_PATHTYPE_DIRECTORY;
-        tmp_info.size = 0;
-    } else {
-        tmp_info.type = SDL_PATHTYPE_OTHER;
-        tmp_info.size = (Uint64) statbuf.st_size;
-    }
+	if (rc < 0) {
+		return SDL_SetError("Can't stat: %s", strerror(errno));
+	}
+	else if (S_ISREG(statbuf.st_mode)) {
+		tmp_info.type = SDL_PATHTYPE_FILE;
+		tmp_info.size = (Uint64) statbuf.st_size;
+	}
+	else if (S_ISDIR(statbuf.st_mode)) {
+		tmp_info.type = SDL_PATHTYPE_DIRECTORY;
+		tmp_info.size = 0;
+	}
+	else {
+		tmp_info.type = SDL_PATHTYPE_OTHER;
+		tmp_info.size = (Uint64) statbuf.st_size;
+	}
 
-    tmp_info.create_time = (SDL_Time)SDL_SECONDS_TO_NS(statbuf.st_ctime);
-    tmp_info.modify_time = (SDL_Time)SDL_SECONDS_TO_NS(statbuf.st_mtime);
-    tmp_info.access_time = (SDL_Time)SDL_SECONDS_TO_NS(statbuf.st_atime);
+	tmp_info.create_time = (SDL_Time) SDL_SECONDS_TO_NS(statbuf.st_ctime);
+	tmp_info.modify_time = (SDL_Time) SDL_SECONDS_TO_NS(statbuf.st_mtime);
+	tmp_info.access_time = (SDL_Time) SDL_SECONDS_TO_NS(statbuf.st_atime);
 
-    if (info) *info = tmp_info;
+	if (info) {
+		*info = tmp_info;
+	}
 
-    return true;
+	return true;
 }
--- ISLE/switch/filesys.h
+++ ISLE/switch/filesys.h
@@ -4,6 +4,6 @@
 #include <SDL3/SDL_filesystem.h>
 
 #define SDL_GetPathInfo NX_GetPathInfo // Override broken SDL_GetPathInfo
-bool NX_GetPathInfo(const char *path, SDL_PathInfo *info);
+bool NX_GetPathInfo(const char* path, SDL_PathInfo* info);
 
 #endif // NX_FILESYS_H
