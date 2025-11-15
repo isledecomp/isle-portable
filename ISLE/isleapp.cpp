328a21ad032f482725981cec16668a500e9cc10f
SnepOMatic (Rhew) <github@rhew.co.uk>
2025-11-15 19:57:17 +0000

    Fix: use Clang-Format 17 instead of 21

--- ISLE/isleapp.cpp
+++ ISLE/isleapp.cpp
@@ -67,6 +67,7 @@
 
 #ifdef __SWITCH__
 #include "switch/config.h"
+
 #include <switch.h>
 #endif
 
@@ -836,17 +837,17 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 		}
 	}
 	else if (event->user.type == g_legoSdlEvents.m_gameEvent) {
-		auto rumble =
-			[](float p_strength, float p_lowFrequencyRumble, float p_highFrequencyRumble, MxU32 p_milliseconds) {
-				if (g_isle->GetHaptic() &&
-					!InputManager()
-						 ->HandleRumbleEvent(p_strength, p_lowFrequencyRumble, p_highFrequencyRumble, p_milliseconds)) {
+		auto rumble = [](float p_strength, float p_lowFrequencyRumble, float p_highFrequencyRumble, MxU32 p_milliseconds
+					  ) {
+			if (g_isle->GetHaptic() &&
+				!InputManager()
+					 ->HandleRumbleEvent(p_strength, p_lowFrequencyRumble, p_highFrequencyRumble, p_milliseconds)) {
 // Platform-specific handling
 #ifdef __EMSCRIPTEN__
-					Emscripten_HandleRumbleEvent(p_lowFrequencyRumble, p_highFrequencyRumble, p_milliseconds);
+				Emscripten_HandleRumbleEvent(p_lowFrequencyRumble, p_highFrequencyRumble, p_milliseconds);
 #endif
-				}
-			};
+			}
+		};
 
 		switch (event->user.code) {
 		case e_hitActor:
